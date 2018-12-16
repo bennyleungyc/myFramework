3abc4d47469ef0c79d45c9ccd03b1202a72ba8fb
Benny Leung <benny.leung.yc@gmail.com>
2018-12-17 00:16:52 +0800

    added and completed implementation of Selenium Helper

--- pages/SearchFlightPage.py
+++ pages/SearchFlightPage.py
@@ -6,6 +6,8 @@ class SearchFlightPage(object):
 
     __url = "http://blazedemo.com/"
     _click_button = find_by(xpath="//input[@type='submit']")
+    _from_port_dropdown = find_by(xpath="//select[@name='fromPort']")
+    _to_port_dropdown = find_by(xpath="//select[@name='toPort']")
 
     def open(self):
         print('SearchFlightPage====open')
@@ -16,14 +18,17 @@ class SearchFlightPage(object):
         self._web = Web(browser)
 
     def select_departure_city(self, city):
-        self._web.get_web_element_by_xpath("//select[@name='fromPort']/option[@value='{}']".format(city)).click()
+        self._web.select_from_dropdown(self._from_port_dropdown(), city)
+        # self._web.get_web_element_by_xpath("//select[@name='fromPort']/option[@value='{}']".format(city)).click()
 
     def select_destination_city(self, city):
-        self._web.get_web_element_by_xpath("//select[@name='toPort']/option[@value='{}']".format(city)).click()
+        self._web.select_from_dropdown(self._to_port_dropdown(), city)
+        # self._web.get_web_element_by_xpath("//select[@name='toPort']/option[@value='{}']".format(city)).click()
 
     def search_for_flights(self):
         # self._web.get_web_element_by_xpath("//input[@type='submit']").click()
-        self._click_button().click()
+        self._web.click_element(self._click_button())
+        # self._click_button().click()
 
     def get_found_flights(self):
         return self._web.get_web_elements_by_xpath("//table[@class='table']/tbody/tr")
--- webdriver/Web.py
+++ webdriver/Web.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver.WebDriverManager import WebDriverManager
-
+from selenium.webdriver.support.ui import Select
 
 class Web(object):
 
@@ -21,6 +21,35 @@ class Web(object):
     def get_web_elements_by_xpath(self, xpath):
         return self._wait.until(EC.presence_of_all_elements_located((By.XPATH,
                                 xpath)))
+
+    def wait_for_element_visible(self, element):
+        return self._wait.until(EC.visibility_of(element))
+
+    def click_element(self, element):
+        self.wait_for_element_visible(element)
+        element.click()
+
+    def send_key_to_element(self, element, text):
+        element.send_keys(text)
+
+    def clear_element_text(self, element):
+        element.clear()
+
+    def clear_and_type(self, element, text):
+        self.clear_element_text(element)
+        self.send_key_to_element(element, text)
+
+    def select_from_dropdown(self, element, text):
+        Select(element).select_by_visible_text(text)
+
+    def is_element_displayed(self, element):
+        return element.is_displayed()
+
+    def is_element_selected(self, element):
+        return element.is_selected()
+
+    def get_text_from_element(self, element):
+        return element.text()
 
     def open(self, path):
         self._driver.get(path)
